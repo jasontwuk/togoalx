7ced0ed577f7e30394b94b186f46515d06f5716b
Jason <jasontwuk@gmail.com>
2024-09-09 08:14:10 +0100

    use Button and Logout components in header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import React from "react";
 import clsx from "clsx";
 import Link from "next/link";
 
 import { kalam } from "../utilities/fonts";
+import { useAuth } from "../utilities/authContext";
+import Logout from "./logout";
 
 export const Header = () => {
+  const { currentUser } = useAuth();
+
   return (
     <div className="flex justify-between items-center p-4 sm:p-8 gap-4">
       <Link
@@ -14,9 +20,18 @@ export const Header = () => {
         ToGoalx
       </Link>
 
-      <button className="py-0.5 px-2 border border-indigo-500 hover:bg-indigo-500 text-indigo-500 hover:text-white rounded-lg duration-200 capitalize font-semibold">
-        log in
-      </button>
+      {currentUser ? (
+        <div className="flex justify-center items-center gap-2">
+          <Logout />
+        </div>
+      ) : (
+        <Link
+          href={"/login"}
+          className="py-0.5 px-2 rounded-full overflow-hidden border border-indigo-500 hover:bg-indigo-500 text-indigo-500 hover:text-white  duration-200 capitalize font-semibold"
+        >
+          log in
+        </Link>
+      )}
     </div>
   );
 };
