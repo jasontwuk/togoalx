9c3817bb70a1cac54a3eb2229755e5a462ad0a3f
Jason <jasontwuk@gmail.com>
2024-09-09 08:10:54 +0100

    add Logout component

---
+++ app/components/logout.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+import { Button } from "./button";
+import { useAuth } from "../utilities/authContext";
+
+function Logout() {
+  const { logout } = useAuth();
+
+  return (
+    <Button clickHandler={logout} className="py-0.5 px-2">
+      Log out
+    </Button>
+  );
+}
+
+export default Logout;
