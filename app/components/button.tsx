cdfaf39148fb958d419fcacb85e8d1c94f29be76
Jason <jasontwuk@gmail.com>
2024-09-11 06:28:02 +0100

    update Button styles

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -4,19 +4,23 @@ import React from "react";
 type ButtonProps = {
   className?: string;
   full?: boolean;
+  dark?: boolean;
   clickHandler?: () => void;
   children: React.ReactNode;
 };
 
 export const Button = (props: ButtonProps) => {
-  const { className, full, clickHandler, children } = props;
+  const { className, full, dark, clickHandler, children } = props;
 
   return (
     <button
       onClick={clickHandler}
       className={clsx(
         full && "w-full",
-        "rounded-full overflow-hidden border border-indigo-500 hover:bg-indigo-500 text-indigo-500 hover:text-white  duration-200 capitalize font-semibold whitespace-nowrap ",
+        dark
+          ? "hover:opacity-80 bg-indigo-700 text-white"
+          : "hover:bg-indigo-500 text-indigo-500 hover:text-white",
+        "rounded-full overflow-hidden border border-indigo-500 duration-200 capitalize font-semibold whitespace-nowrap ",
         className
       )}
     >
