3c4a3e93017d8c7aacb1a4e22e59ac696afdf5ef
Jason <jasontwuk@gmail.com>
2024-09-13 17:16:36 +0100

    add ProgressDonutBar component

---
+++ app/components/ProgressDonutBar.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+type ProgressDonutBarType = {
+  percent: number;
+  emoji: string;
+};
+
+function ProgressDonutBar(props: ProgressDonutBarType) {
+  const { percent, emoji } = props;
+
+  return (
+    <div className="inline-flex items-center justify-center overflow-hidden rounded-full">
+      <svg className="h-20 w-20 -rotate-90">
+        <circle
+          className="text-gray-300"
+          strokeWidth="5"
+          stroke="currentColor"
+          fill="transparent"
+          r="30"
+          cx="40"
+          cy="40"
+        />
+
+        <circle
+          className="text-yellow-500"
+          strokeWidth="5"
+          strokeDasharray={30 * 2 * Math.PI}
+          strokeDashoffset={
+            30 * 2 * Math.PI - (percent / 100) * (30 * 2 * Math.PI)
+          }
+          strokeLinecap="round"
+          stroke="currentColor"
+          fill="transparent"
+          r="30"
+          cx="40"
+          cy="40"
+        />
+      </svg>
+
+      <div className="absolute flex flex-col">
+        <p className="leading-3">{emoji}</p>
+
+        <p className="text-xl text-indigo-700">
+          {percent}
+          <span className="text-xs">%</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ProgressDonutBar;
