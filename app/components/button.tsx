e9e60430cd75b353cabcfb52435d06985548611b
Jason <jasontwuk@gmail.com>
2024-09-18 09:23:29 +0100

    add disabled styles for Button

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -5,23 +5,27 @@ type ButtonProps = {
   className?: string;
   full?: boolean;
   dark?: boolean;
+  disabled?: boolean;
   clickHandler?: () => void;
   children: React.ReactNode;
 };
 
 export const Button = (props: ButtonProps) => {
-  const { className, full, dark, clickHandler, children } = props;
+  const { className, full, dark, disabled, clickHandler, children } = props;
 
   return (
     <button
       onClick={clickHandler}
+      disabled={disabled}
       className={clsx(
         full && "w-full",
         dark
-          ? "hover:opacity-80 bg-indigo-700 text-white"
-          : "hover:bg-indigo-500 text-indigo-500 hover:text-white",
-        "rounded-full overflow-hidden border border-indigo-500 duration-200 capitalize font-semibold whitespace-nowrap ",
-        className
+          ? "bg-indigo-700 text-white hover:opacity-80"
+          : "text-indigo-500 hover:bg-indigo-500 hover:text-white",
+        "overflow-hidden whitespace-nowrap rounded-full border border-indigo-500 font-semibold capitalize duration-200",
+        className,
+        disabled &&
+          "!border-gray-200 !bg-gray-200 !text-gray-500 hover:bg-gray-200 hover:text-gray-500",
       )}
     >
       {children}
