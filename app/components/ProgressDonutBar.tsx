c6b15450795c692ddb2c50b2de69782400ea231c
Jason <jasontwuk@gmail.com>
2024-09-19 11:21:01 +0100

    update styles for ProgressDonutBar

--- app/components/ProgressDonutBar.tsx
+++ app/components/ProgressDonutBar.tsx
@@ -3,12 +3,11 @@ import React from "react";
 
 type ProgressDonutBarType = {
   percent: number;
-  emoji: string;
   loading: boolean;
 };
 
 function ProgressDonutBar(props: ProgressDonutBarType) {
-  const { percent, emoji, loading } = props;
+  const { percent, loading } = props;
 
   if (loading) {
     return (
@@ -42,7 +41,7 @@ function ProgressDonutBar(props: ProgressDonutBarType) {
         <circle
           className={clsx(
             percent >= 80 && "text-green-500",
-            percent < 80 && percent >= 60 && "text-blue-500",
+            percent < 80 && percent >= 60 && "text-sky-500",
             percent < 60 && "text-red-500",
           )}
           strokeWidth="5"
@@ -60,13 +59,19 @@ function ProgressDonutBar(props: ProgressDonutBarType) {
       </svg>
 
       <div className="absolute flex flex-col">
-        <p className="leading-3">{emoji}</p>
+        <p className="-mt-1 leading-3">
+          {percent === 100 ? (
+            <i className="fa-solid fa-star text-yellow-500"></i>
+          ) : (
+            <i className="fa-regular fa-star text-yellow-500"></i>
+          )}
+        </p>
 
-        <p className="text-xl">
+        <p className="text-xl leading-6">
           <span
             className={clsx(
               percent >= 80 && "text-green-700",
-              percent < 80 && percent >= 60 && "text-blue-700",
+              percent < 80 && percent >= 60 && "text-sky-700",
               percent < 60 && "text-red-700",
               "font-semibold",
             )}
