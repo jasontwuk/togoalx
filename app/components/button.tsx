8c7d980a5124b54a3ab4318b867777d0ac713085
Jason <jasontwuk@gmail.com>
2024-09-24 16:12:59 +0100

    refactor Button component

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -18,14 +18,26 @@ export const Button = (props: ButtonProps) => {
       onClick={clickHandler}
       disabled={disabled}
       className={clsx(
+        // *** Note: full width
         full && "w-full",
+
+        // *** Note: dark or not dark version styles
         dark
           ? "bg-indigo-700 text-white hover:opacity-80"
           : "text-indigo-500 hover:bg-indigo-500 hover:text-white",
-        "overflow-hidden whitespace-nowrap rounded-full border border-indigo-500 font-semibold capitalize duration-200 focus-visible:outline focus-visible:outline-[3px] focus-visible:outline-offset-2 focus-visible:outline-yellow-500",
-        className,
+
+        // *** Note: general styles
+        "overflow-hidden whitespace-nowrap rounded-full border border-indigo-500 font-semibold capitalize duration-200",
+
+        // *** Note: focus-visible
+        "focus-visible:outline focus-visible:outline-[3px] focus-visible:outline-offset-2 focus-visible:outline-yellow-500",
+
+        // *** Note: disabled styles
         disabled &&
           "!border-gray-200 !bg-gray-200 !text-gray-500 hover:bg-gray-200 hover:text-gray-500",
+
+        // *** Note: className
+        className,
       )}
     >
       {children}
